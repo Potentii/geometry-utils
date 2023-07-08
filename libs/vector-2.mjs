216c4bb0986b76e6a2388cb59be3b7b12421bb84
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-07-07 22:35:34 -0300

    Added more functions to Vector2

--- libs/vector-2.mjs
+++ libs/vector-2.mjs
@@ -1,3 +1,5 @@
+import {MathUtils} from "@potentii/math-utils";
+
 export default class Vector2{
 
 	/**
@@ -8,6 +10,25 @@ export default class Vector2{
 	constructor(x = 0, y = 0){
 		this.x = x;
 		this.y = y;
+	}
+
+
+	/**
+	 *
+	 * @param {Vector2|{x:number,y:number}|[number, number]} other
+	 * @return {?Vector2}
+	 */
+	static from(other){
+		if(other === null || other === undefined)
+			return null;
+
+		if(Array.isArray(other) && other.length>1)
+			return new Vector2(other[0], other[1]);
+
+		if(typeof other == 'object')
+			return new Vector2(other.x, other.y);
+
+		return null;
 	}
 
 
@@ -184,6 +205,50 @@ export default class Vector2{
 	 */
 	abs(){
 		return new Vector2(Math.abs(this.x), Math.abs(this.y));
+	}
+
+	/**
+	 *
+	 * @param {number} [min=0]
+	 * @param {number} [max=1]
+	 * @return {Vector2}
+	 */
+	clamp(min = 0, max = 1){
+		return new Vector2(
+			MathUtils.clamp(this.x, min, max),
+			MathUtils.clamp(this.y, min, max)
+		);
+	}
+
+	/**
+	 *
+	 * @param {Vector2} [start=Vector2.zero]
+	 * @param {Vector2} [end=Vector2.one]
+	 * @return {Vector2}
+	 */
+	clampArea(start = Vector2.zero, end = Vector2.one){
+		return new Vector2(
+			MathUtils.clamp(this.x, start.x, end.x),
+			MathUtils.clamp(this.y, start.y, end.y)
+		);
+	}
+
+	/**
+	 *
+	 * @param {?(Vector2|{x:number,y:number})|undefined} other
+	 * @return {boolean}
+	 */
+	equals(other){
+		return other !== null && other !== undefined && this.x === other.x && this.y === other.y;
+	}
+
+	/**
+	 *
+	 * @param {?Vector2|undefined} other
+	 * @return {boolean}
+	 */
+	equalsStrict(other){
+		return other !== null && other !== undefined && (other instanceof Vector2) && this.x === other.x && this.y === other.y;
 	}
 
 
